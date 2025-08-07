26bbd729aa7b683d6ba292d10ef5f9e6512fee4b
AyseSudeKurt <aysesude.2000@hotmail.com>
2025-08-07 13:52:06 +0300

    innit

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,7 @@ function App() {
         <Route path="/shop">
           <ShopPage />
         </Route>
-        <Route path="/productDetail">
-          <ProductDetailPage />
-        </Route>
+
       </Switch>
       <Footer />
     </div>
